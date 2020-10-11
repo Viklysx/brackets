fa6165ab233c9a7acfd558cf6bdf544a9e10a517
Viklysx <vikbelec111@yandex.ru>
2020-10-11 05:23:06 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+  while (flag) {
+    let testStr = str.length;
+    bracketsConfig.forEach(element => {
+      let couple = element[0] + element[1];
+      str = str.replace(couple, '');   
+    });
+    flag = testStr === str.length ? false : true;
+  }
+  return (str.length === 0) ? true : false;
 }
